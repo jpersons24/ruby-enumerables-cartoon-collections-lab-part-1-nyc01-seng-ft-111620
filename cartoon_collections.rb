e9caaaf79963aeab194303a369350a5095b49612
Jakob Persons <jakobep24@gmail.com>
2020-10-22 14:46:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,10 +2,20 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+  array.each do |phrase|
+    p "Hello #{phrase}!"
+  end
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  array.each_with_index do |item, index|
+    array[item] = index
+    list_number = index + 1
+    p list_number". #{item}"
+
+
+  end
 end
